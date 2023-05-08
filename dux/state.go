ae7fce5ac83a23630e652f57e14b2eeb547bce89
Jens Green Olander <jens@apels.in>
2023-05-08 20:53:46 +0200

    Move State to state.go

---
+++ dux/state.go
@@ -0,0 +1,22 @@
+package dux
+
+import (
+	"sync"
+
+	"github.com/golang/geo/r2"
+)
+
+type State struct {
+	Treemap        Treemap
+	Quit           bool
+	MaxDepth       int
+	TreemapRect    r2.Rect
+	TotalFiles     int
+	IsWalkingFiles bool
+	Refresh        *sync.Once
+}
+
+type StateUpdate struct {
+	State  State
+	Errors []error
+}

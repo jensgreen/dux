cf98f16e45d9c82aea8e7966fd2025ea6785c742
Jens Green Olander <jens@apels.in>
2023-05-08 21:26:53 +0200

    Remove trailing decimal TODOs
    We want them for visual stability during file walk.

--- files/humanize_test.go
+++ files/humanize_test.go
@@ -1,8 +1,6 @@
 package files
 
-import (
-	"testing"
-)
+import "testing"
 
 func Test_HumanizeIEC(t *testing.T) {
 	tests := []struct {
@@ -14,17 +12,17 @@ func Test_HumanizeIEC(t *testing.T) {
 		{999, "999B"}, // TODO want 1G I guess
 		{1 * Ki, "1.0K"},
 		{2*Ki + 1, "2.0K"},
-		{234 * Ki, "234.0K"}, // TODO want "234K" (no decimal) like in du
-		{Gi - 1, "1024.0M"},  // TODO want 1G I guess
+		{234 * Ki, "234.0K"},
+		{Gi - 1, "1024.0M"}, // TODO want 1G I guess
 		{Gi, "1.0G"},
-		{99*Gi + 1, "99.0G"}, // TODO want "99G" (no decimal) like in du
-		{5 * Ei, "5.0E"},     // int64 does almost overflow here...
+		{99*Gi + 1, "99.0G"},
+		{5 * Ei, "5.0E"}, // int64 does almost overflow here...
 	}
 	for _, tt := range tests {
 		name := tt.want
 		t.Run(name, func(t *testing.T) {
 			if got := HumanizeIEC(tt.n); got != tt.want {
-				t.Errorf("humanize() = %v, want %v", got, tt.want)
+				t.Errorf("humanize(%v) = %v, want %v", tt.n, got, tt.want)
 			}
 		})
 	}
